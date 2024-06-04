7e60547a1f6f90abd6f226282b4cbdabb9872a3d
BhaskarHN18 <baskarhn06@gmail.com>
2024-06-04 12:10:45 +0530

    Add files via upload

---
+++ src/app/sevice/faculty.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Faculty } from '../faculty';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FacultyService {
+  private baseURL="http://localhost:8080/auth/faculty";
+  
+  constructor(private httpClient:HttpClient) { }
+  getFacultyList():Observable<Faculty[]>{
+    return this.httpClient.get<Faculty[]>(`${this.baseURL}`);
+}
+CreateFaculty(faculty: Faculty):Observable<Object>{
+  return this.httpClient.post(`${this.baseURL}`,faculty)
+}
+getFacultyById(id:number):Observable<Faculty>{
+return this.httpClient.get<Faculty>(`${this.baseURL}/${id}`);
+}
+updateEmployee(id: number, faculty: Faculty): Observable<Object>{
+  return this.httpClient.put(`${this.baseURL}/${id}`, faculty);
+}
+deleteFaculty(id: number): Observable<Object>{
+  return this.httpClient.delete(`${this.baseURL}/${id}`);
+}
+}
